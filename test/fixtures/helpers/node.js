ac179ee2ed51938f33f7acc055b5ced5189aff3e
Christopher <christopher.watford@gmail.com>
2014-08-06 15:24:20 -0400

    Adding a default option to break if options not passed
    TravisCI krakenjs/adaro/builds/31842259

--- test/fixtures/helpers/node.js
+++ test/fixtures/helpers/node.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = function (dust, options) {
-    options = options || { greeting: "Hello" };
+    options = options || { greeting: "ASP.Net" };
     dust.helpers.node = function (chunk, ctx, bodies, params) { return chunk.write(options.greeting); };
 };
